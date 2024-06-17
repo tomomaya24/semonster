948d018ffae796393a4aa73eafe6db03ddcd3582
tomomaya24 <e1q21106@oit.ac.jp>
2024-06-17 11:28:46 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("SEMONSTER GAME");
-    System.out.println("Show a new monster!");
+<<<<<<< HEAD
+    System.out.println("SEMonster!");
     Player player = new Player();
     player.drawMonster();
     player.showDeck();
-
-    //System.out.println(monster.toString());
+========
+    System.out.println("SEMONSTER GAME");
+    System.out.println("Show a new monster!");
+    Monster monster = new Monster();
+    System.out.println(monster.toString());
   }
 }
