ac08e23a20e8bdc4adff0d8d685f01aa6f72f013
kazuk <e1q21106@oit.ac.jp>
2024-06-23 23:36:41 +0900

    add Main Player

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
 public static void main(String[] args) {
-    system.out.println("SEMonster!");
+    System.out.println("SEMonster!");
     Player player = new Player();
     player.drawMonsters();
     player.showDeck();
